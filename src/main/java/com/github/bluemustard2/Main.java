22c28c630d6d011ddc55f23a7e4bf82eb2e5c894
Maurice Kennedy <mauricekennedy@Maurices-MacBook-Pro.local>
2020-12-15 00:48:54 -0800

    Version 1.0 (50 quotes total)

---
+++ src/main/java/com/github/bluemustard2/Main.java
@@ -0,0 +1,56 @@
+package com.github.bluemustard2;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.util.logging.FallbackLoggerConfiguration;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    private static String quoteChoice;
+    private static Random rng;
+    private static int index;
+
+    public static void main(String[] args) {
+        // Enable debug logging
+        FallbackLoggerConfiguration.setDebug(true);
+
+        // Enable trace logging
+        FallbackLoggerConfiguration.setTrace(true);
+
+        DiscordApi api = new DiscordApiBuilder()
+                .setToken("Nzg4MjcyNzU1MTExMTAwNDU2.X9hGNg.zEievTzHaKngUdjI-xISp9HAiwE")
+                .login()
+                .join();
+
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!quote")) {
+                event.getChannel().sendMessage(quoteSelector());
+            }
+        });
+    }
+
+    public static String quoteSelector(){
+        File file = new File("quotes.txt");
+        try {
+            Scanner fileReader = new Scanner(file);
+            rng = new Random();
+            ArrayList<String> quoteList = new ArrayList<>();
+
+            while (fileReader.hasNext()){
+                String line = fileReader.nextLine();
+                quoteList.add(line);
+            }
+
+            index = rng.nextInt(quoteList.size()-1);
+            quoteChoice = quoteList.get(index);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return quoteChoice;
+    }
+}
